14d5ff3f1ec513a85618da87239fb4ce0a705ed4
AZAN1205 <156790580+AZAN1205@users.noreply.github.com>
2024-02-06 20:35:44 +0530

    Add files via upload

---
+++ HRM.py
@@ -0,0 +1,19 @@
+class Employee:
+    def __init__(self,Emp_Id,Emp_Name,Emp_Dep,Emp_Contact,Emp_App_Date,Emp_Status):
+        self.Emp_Id = Emp_Id
+        self.Emp_Name = Emp_Name
+        self.Emp_dep = Emp_Dep
+        self.Emp_Contact = Emp_Contact
+        self.Emp_App_Date = Emp_App_Date
+        self.Emp_Status = Emp_Status
+    def add_employee(self,Emp_Id,Emp_Name,Emp_Dep,Emp_Contact,Emp_App_Date,Emp_Status):
+        pass
+    def view_emp_info(self):
+        pass
+    def update_emp_info(self):
+        pass
+    def leave_request(self):
+        pass
+    def view_company_policy(self):
+        pass
+    def
